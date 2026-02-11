0330f8c035c511855d9b8f79868ea10a28dcd822
NullReferenceException <48070081+disahere@users.noreply.github.com>
2026-02-11 14:34:29 +0100

    fixed movement + add camera pitch

--- Assets/CodeBase/_Prototype/Fusion/VeilNetworkRunner.cs
+++ Assets/CodeBase/_Prototype/Fusion/VeilNetworkRunner.cs
@@ -5,61 +5,60 @@ using UnityEngine;
 
 namespace CodeBase._Prototype.Fusion
 {
-    public class VeilNetworkRunner : MonoBehaviour, INetworkRunnerCallbacks
+  public class VeilNetworkRunner : MonoBehaviour, INetworkRunnerCallbacks
+  {
+    [SerializeField] GameMode gameMode = GameMode.Shared;
+    [Space]
+    [SerializeField] NetworkObject playerPrefab;
+
+    readonly string _sessionName = "VeilSession";
+    NetworkRunner _runner;
+
+    async void Start()
     {
-        [SerializeField] GameMode gameMode = GameMode.Shared; // поменять тип после локал тестов
-        
-        [Space]
-        [SerializeField] NetworkObject playerPrefab;
+      _runner = GetComponent<NetworkRunner>();
+      _runner.ProvideInput = true;
+      _runner.AddCallbacks(this);
 
-        private readonly string _sessionName = "VeilSession";
-        private NetworkRunner _runner;
+      await _runner.StartGame(new StartGameArgs
+      {
+        GameMode    = gameMode,
+        SessionName = _sessionName
+      });
 
-        private async void Start()
-        {
-            _runner = GetComponent<NetworkRunner>();
-            _runner.ProvideInput = true;
-            _runner.AddCallbacks(this);
-
-            await _runner.StartGame(new StartGameArgs
-            {
-                GameMode    = gameMode,
-                SessionName = _sessionName
-            });
-
-            if (_runner.IsSharedModeMasterClient) // вынести в модуль
-            {
-                _ = _runner.LoadScene(Constants.TestSceneName);
-            }
-        }
-        
-        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-        {
-            Debug.Log($"OnPlayerJoined {player}, IsServer={runner.IsServer}, IsMaster={runner.IsSharedModeMasterClient}");
-
-            if (runner.IsSharedModeMasterClient)
-            {
-                runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, player);
-            }
-        }
-        
-        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
-        public void OnInput(NetworkRunner runner, NetworkInput input) { }
-        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
-        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
-        public void OnConnectedToServer(NetworkRunner runner) { }
-        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
-        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
-        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
-        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
-        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
-        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
-        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
-        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, System.ArraySegment<byte> data) { }
-        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
-        public void OnSceneLoadDone(NetworkRunner runner) { }
-        public void OnSceneLoadStart(NetworkRunner runner) { }
-        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
-        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+      if (_runner.IsSharedModeMasterClient)
+      {
+        _ = _runner.LoadScene(Constants.TestSceneName);
+      }
     }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+      Debug.Log($"OnPlayerJoined {player}, IsServer={runner.IsServer}, IsMaster={runner.IsSharedModeMasterClient}");
+
+      if (runner.IsSharedModeMasterClient)
+      {
+        runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, player);
+      }
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, System.ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+  }
 }
--- Assets/CodeBase/_Prototype/Movement/VeilPlayerController.cs
+++ Assets/CodeBase/_Prototype/Movement/VeilPlayerController.cs
@@ -7,9 +7,18 @@ namespace CodeBase._Prototype.Movement
   {
     [SerializeField] float moveSpeed = 5f;
 
+    [Header("Camera")]
+    [SerializeField] Camera playerCamera;
+    [SerializeField] Transform cameraRoot;
+    [SerializeField] float mouseSensitivity = 0.1f;
+    [SerializeField] float minPitch = -80f;
+    [SerializeField] float maxPitch = 80f;
+
     CharacterController _controller;
     VeilInputActions _input;
     Vector2 _moveInput;
+    Vector2 _lookInput;
+    float _pitch;
 
     public override void Spawned()
     {
@@ -17,11 +26,31 @@ namespace CodeBase._Prototype.Movement
 
       if (Object.HasInputAuthority)
       {
+        if (playerCamera != null)
+        {
+          playerCamera.gameObject.SetActive(true);
+          playerCamera.enabled = true;
+        }
+
         _input = new VeilInputActions();
         _input.Player.Enable();
 
         _input.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
-        _input.Player.Move.canceled  += _ => _moveInput = Vector2.zero;
+        _input.Player.Move.canceled += _ => _moveInput = Vector2.zero;
+
+        _input.Player.Look.performed += ctx => _lookInput = ctx.ReadValue<Vector2>();
+        _input.Player.Look.canceled += _ => _lookInput = Vector2.zero;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+      }
+      else
+      {
+        if (playerCamera != null)
+        {
+          playerCamera.enabled = false;
+          playerCamera.gameObject.SetActive(false);
+        }
       }
     }
 
@@ -31,18 +60,49 @@ namespace CodeBase._Prototype.Movement
       {
         _input.Player.Disable();
         _input.Dispose();
+        _input = null;
       }
     }
 
     public override void FixedUpdateNetwork()
     {
-      if (!Object.HasInputAuthority) return;
+      if (!Object.HasInputAuthority || _controller == null)
+        return;
+      
+      Vector3 worldInput = new Vector3(_moveInput.x, 0f, _moveInput.y);
+      if (worldInput.sqrMagnitude > 1f)
+        worldInput.Normalize();
+      
+      Vector3 localMove = transform.TransformDirection(worldInput);
+      Vector3 move = localMove * moveSpeed * Runner.DeltaTime;
 
-      Vector3 input = new Vector3(_moveInput.x, 0f, _moveInput.y);
-      if (input.sqrMagnitude > 1f) input.Normalize();
-
-      Vector3 move = transform.TransformDirection(input) * moveSpeed * Runner.DeltaTime;
       _controller.Move(move);
+    }
+
+    void Update()
+    {
+      if (!Object.HasInputAuthority)
+        return;
+
+      HandleLook();
+    }
+
+    void HandleLook()
+    {
+      if (cameraRoot == null)
+        return;
+
+      float mouseX = _lookInput.x * mouseSensitivity;
+      float mouseY = _lookInput.y * mouseSensitivity;
+      
+      transform.Rotate(Vector3.up, mouseX);
+      
+      _pitch -= mouseY;
+      _pitch = Mathf.Clamp(_pitch, minPitch, maxPitch);
+
+      Vector3 euler = cameraRoot.localEulerAngles;
+      euler.x = _pitch;
+      cameraRoot.localEulerAngles = euler;
     }
   }
 }
